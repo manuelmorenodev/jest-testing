92511d15131f37e00951e501380b2dd54e921341
Manuel Moreno <manuelmorenodev@gmail.com>
2022-03-03 15:58:01 +0100

    first commit

---
+++ multi.test.js
@@ -0,0 +1,13 @@
+const multi = require("./multi");
+
+describe("Testing multi function", () => {
+  it("Should exists", () => {
+    expect(typeof multi).toBe("function");
+  });
+  it("Should return 6 for 2, 3", () => {
+    expect(multi(2, 3)).toBe(6);
+  });
+  it("Should return 4 for 2, 2", () => {
+    expect(multi(2, 2)).toBe(4);
+  });
+});
